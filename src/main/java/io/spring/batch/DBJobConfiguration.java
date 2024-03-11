dcb8b2b4fe598a3437708ff67ab9c403abb162ae
ghdcksgml1 <ghdcksgml2@naver.com>
2024-03-11 23:36:11 +0900

    feat: section2

---
+++ src/main/java/io/spring/batch/DBJobConfiguration.java
@@ -0,0 +1,49 @@
+package io.spring.batch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class DBJobConfiguration {
+
+    private final JobRepository jobRepository;
+    private final JdbcTransactionManager platformTransactionManager;
+
+    @Bean
+    public Job job() {
+        return new JobBuilder("job", jobRepository)
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Step1 was executed");
+                    return RepeatStatus.FINISHED;
+                }, platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Step2 was executed");
+                    return RepeatStatus.FINISHED;
+                }, platformTransactionManager)
+                .build();
+    }
+}
