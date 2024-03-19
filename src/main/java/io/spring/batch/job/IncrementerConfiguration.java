d2fe67fbbf581fce21fffe5ab3f8f0bf34ec995c
Chuck(홍찬희) <chhong@gabia.com>
2024-03-20 08:00:24 +0900

    feat: section5

--- src/main/java/io/spring/batch/job/IncrementerConfiguration.java
+++ src/main/java/io/spring/batch/job/IncrementerConfiguration.java
@@ -1,48 +1,48 @@
-package io.spring.batch.job;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
-
-@Configuration
-@RequiredArgsConstructor
-public class IncrementerConfiguration {
-    private final JobRepository jobRepository;
-    private final PlatformTransactionManager platformTransactionManager;
-
-    @Bean
-    public Job job() {
-        return new JobBuilder("Job", jobRepository)
-                .start(step1())
-                .next(step2())
-                .incrementer(new CustomJobParametersIncrementer())
-                .build();
-    }
-
-    @Bean
-    public Step step1() {
-        return new StepBuilder("step1", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step1 was executed");
-                    return RepeatStatus.FINISHED;
-                }, platformTransactionManager)
-                .build();
-    }
-
-    @Bean
-    public Step step2() {
-        return new StepBuilder("step2", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step2 was executed");
-                    return RepeatStatus.FINISHED;
-                }, platformTransactionManager)
-                .build();
-    }
-}
+//package io.spring.batch.job;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.batch.core.Job;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.job.builder.JobBuilder;
+//import org.springframework.batch.core.repository.JobRepository;
+//import org.springframework.batch.core.step.builder.StepBuilder;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class IncrementerConfiguration {
+//    private final JobRepository jobRepository;
+//    private final PlatformTransactionManager platformTransactionManager;
+//
+//    @Bean
+//    public Job job() {
+//        return new JobBuilder("Job", jobRepository)
+//                .start(step1())
+//                .next(step2())
+//                .incrementer(new CustomJobParametersIncrementer())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step1() {
+//        return new StepBuilder("step1", jobRepository)
+//                .tasklet((contribution, chunkContext) -> {
+//                    System.out.println("step1 was executed");
+//                    return RepeatStatus.FINISHED;
+//                }, platformTransactionManager)
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step2() {
+//        return new StepBuilder("step2", jobRepository)
+//                .tasklet((contribution, chunkContext) -> {
+//                    System.out.println("step2 was executed");
+//                    return RepeatStatus.FINISHED;
+//                }, platformTransactionManager)
+//                .build();
+//    }
+//}
