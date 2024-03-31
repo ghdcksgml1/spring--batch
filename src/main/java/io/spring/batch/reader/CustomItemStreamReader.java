0d8ec6c5ad73bae92feeb72e5cf82e5458c8f80c
Chuck(홍찬희) <chhong@gabia.com>
2024-04-01 06:47:52 +0900

    feat: section7 finish

--- src/main/java/io/spring/batch/reader/CustomItemStreamReader.java
+++ src/main/java/io/spring/batch/reader/CustomItemStreamReader.java
@@ -1,52 +1,52 @@
-package io.spring.batch.reader;
-
-import org.springframework.batch.item.*;
-
-import java.util.List;
-
-public class CustomItemStreamReader implements ItemStreamReader<String> {
-    private static final String INDEX_KEY = "index";
-    private final List<String> items;
-    private int index = -1;
-    private boolean restart = false;
-
-    public CustomItemStreamReader(List<String> items) {
-        this.items = items;
-        this.index = 0;
-    }
-
-    @Override
-    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-        String item = null;
-        if (this.index < this.items.size()) {
-            System.out.println("read = " + index);
-            item = this.items.get(index++);
-        }
-
-        if (this.index == 7 && !restart) {
-            throw new RuntimeException("Restart is required!");
-        }
-        return item;
-    }
-
-    @Override
-    public void open(ExecutionContext executionContext) throws ItemStreamException {
-        if (executionContext.containsKey(INDEX_KEY)) {
-            index = executionContext.getInt(INDEX_KEY);
-            this.restart = true;
-        } else {
-            index = 0;
-            executionContext.put(INDEX_KEY, index);
-        }
-    }
-
-    @Override
-    public void update(ExecutionContext executionContext) throws ItemStreamException {
-        executionContext.put(INDEX_KEY, index);
-    }
-
-    @Override
-    public void close() throws ItemStreamException {
-        ItemStreamReader.super.close();
-    }
-}
+//package io.spring.batch.reader;
+//
+//import org.springframework.batch.item.*;
+//
+//import java.util.List;
+//
+//public class CustomItemStreamReader implements ItemStreamReader<String> {
+//    private static final String INDEX_KEY = "index";
+//    private final List<String> items;
+//    private int index = -1;
+//    private boolean restart = false;
+//
+//    public CustomItemStreamReader(List<String> items) {
+//        this.items = items;
+//        this.index = 0;
+//    }
+//
+//    @Override
+//    public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+//        String item = null;
+//        if (this.index < this.items.size()) {
+//            System.out.println("read = " + index);
+//            item = this.items.get(index++);
+//        }
+//
+//        if (this.index == 7 && !restart) {
+//            throw new RuntimeException("Restart is required!");
+//        }
+//        return item;
+//    }
+//
+//    @Override
+//    public void open(ExecutionContext executionContext) throws ItemStreamException {
+//        if (executionContext.containsKey(INDEX_KEY)) {
+//            index = executionContext.getInt(INDEX_KEY);
+//            this.restart = true;
+//        } else {
+//            index = 0;
+//            executionContext.put(INDEX_KEY, index);
+//        }
+//    }
+//
+//    @Override
+//    public void update(ExecutionContext executionContext) throws ItemStreamException {
+//        executionContext.put(INDEX_KEY, index);
+//    }
+//
+//    @Override
+//    public void close() throws ItemStreamException {
+//        ItemStreamReader.super.close();
+//    }
+//}
--- src/main/java/io/spring/batch/reader/ItemStreamConfiguration.java
+++ src/main/java/io/spring/batch/reader/ItemStreamConfiguration.java
@@ -1,66 +1,75 @@
-package io.spring.batch.reader;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@Configuration
-@RequiredArgsConstructor
-public class ItemStreamConfiguration {
-
-    private final JobRepository jobRepository;
-    private final PlatformTransactionManager platformTransactionManager;
-
-    @Bean
-    public Job job() {
-        return new JobBuilder("job", jobRepository)
-                .start(step1())
-                .next(step2())
-                .build();
-    }
-
-    @Bean
-    public Step step1() {
-        return new StepBuilder("step1", jobRepository)
-                .<String, String>chunk(5, platformTransactionManager)
-                .reader(itemReader())
-                .writer(itemWriter())
-                .build();
-    }
-
-    public CustomItemStreamReader itemReader() {
-        int itemCount = 10;
-        List<String> items = new ArrayList<>(itemCount);
-
-        for (int i = 0; i < itemCount; i++) {
-            items.add(String.valueOf(i));
-        }
-
-        return new CustomItemStreamReader(items);
-    }
-
-    public ItemWriter<? super String> itemWriter() {
-        return new CustomItemStreamWriter();
-    }
-
-    @Bean
-    public Step step2() {
-        return new StepBuilder("step2", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("Step2 has executed!");
-                    return RepeatStatus.FINISHED;
-                }, platformTransactionManager)
-                .build();
-    }
-}
+//package io.spring.batch.reader;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.batch.core.*;
+//import org.springframework.batch.core.job.builder.JobBuilder;
+//import org.springframework.batch.core.repository.JobRepository;
+//import org.springframework.batch.core.step.builder.StepBuilder;
+//import org.springframework.batch.item.Chunk;
+//import org.springframework.batch.item.ItemProcessor;
+//import org.springframework.batch.item.ItemWriter;
+//import org.springframework.batch.repeat.RepeatStatus;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Random;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class ItemStreamConfiguration {
+//
+//    private final JobRepository jobRepository;
+//    private final PlatformTransactionManager platformTransactionManager;
+//
+//    @Bean
+//    public Job job() {
+//        return new JobBuilder("job", jobRepository)
+//                .start(step1())
+//                .next(step2())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Step step1() {
+//        return new StepBuilder("step1", jobRepository)
+//                .<String, String>chunk(5, platformTransactionManager)
+//                .reader(itemReader())
+//                .processor(new ItemProcessor<String, String>() {
+//                    @Override
+//                    public String process(String item) throws Exception {
+//                        System.out.println("processing = " + item);
+//                        return item;
+//                    }
+//                })
+//                .writer(itemWriter())
+//                .build();
+//    }
+//
+//    public CustomItemStreamReader itemReader() {
+//        int itemCount = 10;
+//        List<String> items = new ArrayList<>(itemCount);
+//
+//        for (int i = 0; i < itemCount; i++) {
+//            items.add(String.valueOf(i));
+//        }
+//
+//        return new CustomItemStreamReader(items);
+//    }
+//
+//    public ItemWriter<? super String> itemWriter() {
+//        return new CustomItemStreamWriter();
+//    }
+//
+//    @Bean
+//    public Step step2() {
+//        return new StepBuilder("step2", jobRepository)
+//                .tasklet((contribution, chunkContext) -> {
+//                    System.out.println("Step2 has executed!");
+//                    return RepeatStatus.FINISHED;
+//                }, platformTransactionManager)
+//                .build();
+//    }
+//}
