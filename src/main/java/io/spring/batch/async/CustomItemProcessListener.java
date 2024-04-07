e1e91d15b0ea0977ab243fca1b78a231df7a9b7a
Chuck(홍찬희) <chhong@gabia.com>
2024-04-08 07:55:46 +0900

    feat: section12

---
+++ src/main/java/io/spring/batch/async/CustomItemProcessListener.java
@@ -0,0 +1,21 @@
+package io.spring.batch.async;
+
+import org.springframework.batch.core.ItemProcessListener;
+
+public class CustomItemProcessListener implements ItemProcessListener<String, String> {
+
+    @Override
+    public void beforeProcess(String item) {
+        ItemProcessListener.super.beforeProcess(item);
+    }
+
+    @Override
+    public void afterProcess(String item, String result) {
+        System.out.println("Thread : " + Thread.currentThread().getName() + ", process item : " + item);
+    }
+
+    @Override
+    public void onProcessError(String item, Exception e) {
+        ItemProcessListener.super.onProcessError(item, e);
+    }
+}
---
+++ src/main/java/io/spring/batch/async/CustomItemReadListener.java
@@ -0,0 +1,21 @@
+package io.spring.batch.async;
+
+import org.springframework.batch.core.ItemReadListener;
+
+public class CustomItemReadListener implements ItemReadListener<String> {
+
+    @Override
+    public void beforeRead() {
+        ItemReadListener.super.beforeRead();
+    }
+
+    @Override
+    public void afterRead(String item) {
+        System.out.println("Thread : " + Thread.currentThread().getName() + ", read item : " + item);
+    }
+
+    @Override
+    public void onReadError(Exception ex) {
+        ItemReadListener.super.onReadError(ex);
+    }
+}
